d755a0a38775c31e0cafa5139f67612543999e1d
Amro Abdalla <amroadel@aucegypt.edu>
2021-08-25 03:16:43 +0200

    Update 3d_resnet_lstm.py
    0

--- steering-control/3d_resnet_lstm.py
+++ steering-control/3d_resnet_lstm.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
-"""Loads a sample video and classifies using a trained Kinetics checkpoint."""
+"""Loads a sample video and classifies using a trained Kinetics checkpoint.0"""
 
 from __future__ import absolute_import
 from __future__ import division
